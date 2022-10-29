c8533ac3909f2eed787fd0ed587dab8cf27ff063
phi1ny3 <38825543+phi1ny3@users.noreply.github.com>
2022-10-29 14:20:51 -0600

    including position files

--- Artillery/Artillery/mathFunctions.cpp
+++ Artillery/Artillery/mathFunctions.cpp
@@ -2,138 +2,209 @@
 //
 
 #include <iostream>
+#include "mathFunctions.h"
+#include <math.h>
+#include <cmath>
+#include <position.h>
+#include <position.cpp>
 using namespace std;
-class mathFunctions {
-private:
-	double distance;
-	double hangtime;
-	double degrees;
-	double radians;
-public:
-	// Setters
-	void setDegrees(double input) {
-		degrees = input;
-	}
-	void setRadians(double input) {
-		radians = input;
-	}
-	void setDistance(double input) {
-		distance = input;
-	}
-	void setHangtime(double input) {
-		hangtime = input;
-	}
 
-	// Getters
-	double getDegrees() {
-		return degrees;
-	}
-	double getDistance() {
-		return distance;
-	}
-	double getHangtime() {
-		return hangtime;
-	}
+// Setters
+void mathFunctions :: setDegrees(double input)
+{
+	degrees = input;
+}
+	
+void mathFunctions :: setRadians(double input)
+{
+	radians = input;
+}
 
-	// Equations
-	void convertToDegrees(double input) {
-		/*
-			r = angle in radians
-			π = pi, about 3.14159
-			d = angle in degrees
-		*/
-		// 	r / 2 π = d / 360
-		double pi = 2 * acos(0.0);
-		double output = (360) * (input / 2 * pi);
-		setDegrees(output);
-	}
-	void convertToRadians(double input) {
-		/*
-			r = angle in radians
-			π = pi, about 3.14159
-			d = angle in degrees
-		*/
-		// 	r / 2 π = d / 360
-		double pi = 2 * acos(0.0);
-		double output = (360) * (input / 2 * pi);
-		setRadians(output);
-	}
-	double verticalComponentSpeed(double speed, double direction) {
-		/*
-			dy = vertical component of speed(m / s)
-			s = overall speed(m / s)
-			a = direction of travel where 0 is pointing up(radians)
-		*/
-		// dy = s cos(a)
-		double dy = speed * cos(direction);
-		return dy;
-	}
-	double HorizontalComponentSpeed(double speed, double direction) {
-		/*
-			dx = vertical component of speed (m/s)
-			s = overall speed (m/s)
-			a = direction of travel where 0 is pointing up (radians)
-		*/
-		// dx = s sin(a)
-		double dx = speed + sin(direction);
-		return dx;
-	}
-	double AngleFromComponents(double dx, double dy) {
-		/*
-			a = directon of travel where 0 is pointing up (radians)
-			dx = horizontal component of speed (m/s)
-			dy = vertical component of speed (m/s)
-		*/
-		// 	a = atan(dx, dy)
-		double output;
-		//output = atan(dx, dy);
-		return output;
-	}
-	double pythagoreanTheorem(double dx, double dy) {
-		/*
-			dx = horizontal component of speed (m/s)
-			dy = vertical component of speed (m/s)
-			s = overall speed (m/s)
-		*/
-		// dx^2 + dy^2 = s2
-		double speed = (dx * dx) + (dy * dy);
-		return speed * speed;
-	}
-	double interpoleration(double r0, double d0, double r1, double d1) {
+void mathFunctions :: setDistance(double input) {
+	distance = input;
+}
 
-		// TODO: Not done
-		/*
-			d0,r0 = coordinates of one point
-			d1,r1 = coordinates of a second point
-			d,r = coordinates of a point in the middle
-		*/
-		// (r - r0) / (d - d0) = (r1 - r0) / (d1 - d0)
-		double d;
-		double r = ((((r1 - r0) / (d1 - d0)) * (d - d0)) - r0);
+void mathFunctions :: setHangtime(double input) {
+	hangtime = input;
+}
 
-	}
-};
+// Getters
+double mathFunctions :: getDegrees() {
+	return degrees;
+}
+double mathFunctions :: getDistance() {
+	return distance;
+}
+double mathFunctions :: getHangtime() {
+	return hangtime;
+}
+
+// Equations
+void mathFunctions :: convertToDegrees(double input)
+{
+	/*
+		r = angle in radians
+		π = pi, about 3.14159
+		d = angle in degrees
+	*/
+	// 	r / 2 π = d / 360
+	double pi = 2 * acos(0.0);
+	double output = (360) * (input / 2 * pi);
+	setDegrees(output);
+}
+void mathFunctions :: convertToRadians(double input) {
+	/*
+		r = angle in radians
+		π = pi, about 3.14159
+		d = angle in degrees
+	*/
+	// 	r / 2 π = d / 360
+	double pi = 2 * acos(0.0);
+	double output = (360) * (input / 2 * pi);
+	setRadians(output);
+}
+double mathFunctions :: verticalComponentSpeed(double speed, double direction) {
+	/*
+		dy = vertical component of speed(m / s)
+		s = overall speed(m / s)
+		a = direction of travel where 0 is pointing up(radians)
+	*/
+	// dy = s cos(a)
+	double dy = speed * cos(direction);
+	return dy;
+}
+double mathFunctions :: HorizontalComponentSpeed(double speed, double direction)
+{
+	/*
+		dx = vertical component of speed (m/s)
+		s = overall speed (m/s)
+		a = direction of travel where 0 is pointing up (radians)
+	*/
+	// dx = s sin(a)
+	double dx = speed + sin(direction);
+	return dx;
+}
+double mathFunctions :: AngleFromComponents(double dx, double dy) {
+	/*
+		a = directon of travel where 0 is pointing up (radians)
+		dx = horizontal component of speed (m/s)
+		dy = vertical component of speed (m/s)
+	*/
+	//a = atan(dx, dy)
+	double output;
+	output = atan2 (dx, dy);
+	return output;
+}
+double mathFunctions :: pythagoreanTheorem(double dx, double dy) {
+	/*
+		dx = horizontal component of speed (m/s)
+		dy = vertical component of speed (m/s)
+		s = overall speed (m/s)
+	*/
+	// dx^2 + dy^2 = s2
+	double speed = (dx * dx) + (dy * dy);
+
+	return speed, dx;
+}
+double mathFunctions :: interpolation(double input = 200) {
+
+	// TODO: Not done
+	/*
+		d0,r0 = coordinates of one point
+		d1,r1 = coordinates of a second point
+		d,r = coordinates of a point in the middle
+	*/
+	// (r - r0) / (d - d0) = (r1 - r0) / (d1 - d0)
+	//(r - 1.225)/(d - 0) = (1.112 - 1.225) / (1000 - 0)
+	double d0 = 0.0;
+	double r0 = 1.225
+	double d1 = 1000;
+	double r1 = 1.112;
+	double d = input;
+	double slope = ((r1 - r0) / (d1 - d0));
+	double r = r0 + slope * (d - d0);
+	return d, r;
+}
+double mathFunctions :: Force(double acceleration = 827){
+	/*
+	f = force in newtons (N)
+	m = mass in kilograms (kg)
+	a = acceleration (m/s2)
+	*/
+	//f = m a
+	double force = 46.7 * acceleration;
+	return force;
+}
+double mathFunctions :: AreaOfCircle(){
+	/*
+	a = area of a circle (m2)
+	r = radius of a circle
+	*/
+	//a = π r 2
+	double pi = 2 * acos(0.0);
+	double r = (154.89 / 2);
+	double area = pi * (r * r);
+}
+double mathFunctions :: DragForceEquation(double c, double p, double v, double a){
+	/*
+	d = force in newtons (N)
+	c = drag coefficient
+	ρ = density of the fluid/gas
+	v = velocity of the projectile
+	a = surface area
+	*/
+	//d = ½ c ρ v2 a
+	double drag = .5 * c * p * (v * v) * a;
+	return drag;
+}
+
+float Position :: computeDistance(const Position& pos1, const Position& pos2)
+{
+	return sqrt((pos1.getMetersX() - pos2.getMetersX()) * (pos1.getMetersX() - pos2.getMetersX()) +
+		(pos1.getMetersY() - pos2.getMetersY()) * (pos1.getMetersY() - pos2.getMetersY()));
+}
+
 int main()
 {
     // Test case up
-	cout << "What is the angle of the howitzer where 0 is up (Degrees)? " << 0 << endl;
+	cout << "What is the angle of the howitzer where 0 is up (Degrees)? " << endl;
 	mathFunctions test1;
 	test1.setDegrees(0.0);
+	cout << test1.getDegrees() << endl;
+	cout << "Distance:      ";
+	cout << test1.computeDistance(pos1, pos2) << "       ";
+	cout << "Hang Time:     ";
+	//cout << test1.DragForceEquation() << endl;
 
 	// Test case high
-	cout << "What is the angle of the howitzer where 0 is up (Degrees)? " << 30 << endl;
+	cout << "What is the angle of the howitzer where 0 is up (Degrees)? " << endl;
 	mathFunctions test2;
 	test2.setDegrees(30.0);
+	cout << test2.getDegrees() << endl;
+	cout << "Distance:      ";
+	cout << test1.computeDistance(pos1, pos2) << "       ";
+	cout << "Hang Time:     ";
+	//cout << test1.DragForceEquation() << endl;
 
 	// Test case low
-	cout << "What is the angle of the howitzer where 0 is up (Degrees)? " << 60 << endl;
+	cout << "What is the angle of the howitzer where 0 is up (Degrees)? " << endl;
 	mathFunctions test3;
 	test3.setDegrees(60.0);
+	cout << test3.getDegrees() << endl;
+	cout << "Distance:      ";
+	cout << test1.computeDistance(pos1, pos2) << "       ";
+	cout << "Hang Time:     ";
+	//cout << test1.DragForceEquation() << endl;
 
 	// Test case backwards
-	cout << "What is the angle of the howitzer where 0 is up (Degrees)? " << -45 << endl;
+	cout << "What is the angle of the howitzer where 0 is up (Degrees)? " << endl;
 	mathFunctions test4;
 	test4.setDegrees(-45.0);
+	cout << test4.getDegrees() << endl;
+	cout << "Distance:      ";
+	cout << test1.computeDistance(pos1, pos2) << "       ";
+	cout << "Hang Time:     ";
+	//cout << test1.DragForceEquation() << endl;
 
 	return 0;
 }
--- Artillery/Artillery/mathFunctions.h
+++ Artillery/Artillery/mathFunctions.h
@@ -1,15 +1,18 @@
 #pragma once
 
+
 class mathFunctions {
 private:
 	double distance;
 	double hangtime;
-	int degrees;
+	double degrees;
+	double radians;
 public:
 	// Setters
 	void setDegrees(double input);
 	void setDistance(double input);
 	void setHangtime(double input);
+	void setRadians(double input);
 
 	// Getters
 	double getDegrees();
@@ -18,10 +21,14 @@ public:
 
 	// Math Functions
 	void convertToDegrees(double input);
+	void convertToRadians(double input);
+	double DragForceEquation(double dragco, double density, double velocity, double surfaceArea);
+	double AreaOfCircle();
+	double Force(double acceleration);
 	double verticalComponentSpeed(double speed, double direction);
 	double HorizontalComponentSpeed(double speed, double direction);
 	double AngleFromComponents(double dx, double dy);
 	double pythagoreanTheorem(double dx, double dy);
-	double interpoleration(double r0, double d0, double r1, double d1);
+	double interpolation(double r0, double d0, double r1, double d1);
 
 };
